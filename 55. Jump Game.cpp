d30863230f15a171ce0cbf78b3f45e1d214b47f7
asingh072318 <ankit.bitmsra@gmail.com>
2022-01-16 21:59:09 -0500

    55. Jump Game.cpp

--- 55. Jump Game.cpp
+++ 55. Jump Game.cpp
@@ -20,3 +20,16 @@ public:
         return false;
     }
 };
+
+## Another Solution(Greedy)
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size(),goal = n-1;
+        for(int i=n-2;i>=0;i--){
+            if(i+nums[i]>=goal)
+                goal = i;
+        }
+        return goal==0;
+    }
+};
