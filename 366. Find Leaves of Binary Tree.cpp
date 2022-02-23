d46de707ecbcbb8850b9f2d079160b8f3b656b3a
asingh072318 <ankit.bitmsra@gmail.com>
2022-02-22 20:13:16 -0500

    366. Find Leaves of Binary Tree

---
+++ 366. Find Leaves of Binary Tree.cpp
@@ -0,0 +1,43 @@
+## https://leetcode.com/problems/find-leaves-of-binary-tree/
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    
+    int getHeight(TreeNode* root, vector<pair<int,int>>& height){
+        if(root == NULL)
+            return -1;
+        int lheight = getHeight(root->left,height);
+        int rheight = getHeight(root->right,height);
+        int cheight = 1 + max(lheight,rheight);
+        height.push_back(make_pair(cheight,root->val));
+        return cheight;
+    }
+    
+    vector<vector<int>> findLeaves(TreeNode* root) {
+        vector<pair<int,int>> height;
+        getHeight(root,height);
+        sort(height.begin(),height.end());
+        vector<vector<int>> response;
+        vector<int> curr;
+        curr.push_back(height[0].second);
+        for(int i=1;i<height.size();i++){
+            if(height[i].first != height[i-1].first){
+                response.push_back(curr);
+                curr.clear();
+            }
+            curr.push_back(height[i].second);
+        }
+        response.push_back(curr);
+        return response;
+    }
+};
