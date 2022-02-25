0e60edcdffb7035e37f7c4bc803973410337c0b5
asingh072318 <ankit.bitmsra@gmail.com>
2022-02-25 00:12:22 -0500

    Added another implementation

--- 314. Binary Tree Vertical Order Traversal.cpp
+++ 314. Binary Tree Vertical Order Traversal.cpp
@@ -39,3 +39,42 @@ public:
         return response;
     }
 };
+
+
+// Implementation with map instead of unordered_map. No need of column vector here.
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> verticalOrder(TreeNode* root) {
+        vector<vector<int>> response;
+        queue<pair<TreeNode*,int>> q;
+        map<int,vector<int>> mp;
+        int col = 0;
+        q.push(make_pair(root,col));
+        while(!q.empty()){
+            pair<TreeNode*,int> curr = q.front();
+            q.pop();
+            root = curr.first;
+            col = curr.second;
+            if(root != NULL){
+                mp[col].push_back(root->val);
+                q.push(make_pair(root->left,col-1));
+                q.push(make_pair(root->right,col+1));   
+            }
+        }
+        for(auto it = mp.begin();it!= mp.end();it++){
+            response.push_back(mp[it->first]);
+        }
+        return response;
+    }
+};
